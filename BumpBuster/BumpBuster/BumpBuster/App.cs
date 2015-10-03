52f2d2220c9dafec98ed9f959875c27ef0d98d2d
Bruno Vieira <brunovlandim@hotmail.com>
2015-10-03 16:44:50 -0300

    Criação da main page

--- BumpBuster/BumpBuster/BumpBuster/App.cs
+++ BumpBuster/BumpBuster/BumpBuster/App.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BumpBuster.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,19 +13,7 @@ namespace BumpBuster
         public App()
         {
             // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        new Label {
-                            XAlign = TextAlignment.Center,
-                            Text = "Welcome to Xamarin Forms!"
-                        }
-                    }
-                }
-            };
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
---
+++ BumpBuster/BumpBuster/BumpBuster/Views/MainPage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BumpBuster.Views
+{
+    public class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            this.Content = new StackLayout
+            {
+                VerticalOptions = LayoutOptions.Center,
+                Children = {
+                    new Label {
+                        XAlign = TextAlignment.Center,
+                        Text = "Welcome to Xamarin Forms!"
+                    }
+                }
+            };
+        }
+    }
+}
